dd212eb147dc987251458e3bcb3437e678d4526d
Inem19 <117159646+Inem19@users.noreply.github.com>
2023-01-23 16:15:55 +0000

    changed apikey for weather forecast

--- src/index.js
+++ src/index.js
@@ -120,7 +120,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "40b745c14eadad7b7c4e6e4bf3b70103";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
